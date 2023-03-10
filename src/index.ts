d18facf7c0ce8561c28f9ba5ed21d9757b780aef
empolem33 <inspiron0925@yahoo.com>
2023-03-09 16:29:25 -0800

    error

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+class User {
+    constructor(public email:string, readonly name:string){
+        
+    }
+}
+
+const mm = new User('m@m.com' ,'marco')
---
+++ src/myGenerics.ts
@@ -0,0 +1,10 @@
+interface Admin {
+    email: string,
+    name: string
+}
+
+interface User {
+    email: string,
+    name: string
+}
+
